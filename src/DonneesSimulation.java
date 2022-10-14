d0219d64ad1717b5a25edf71a655aac319e8a626
Canis <robin.canis@phelma.grenoble-inp.fr>
2022-10-14 16:43:26 +0200

    ajout DonneesSimulation

--- src/DonneesSimulation.java
+++ src/DonneesSimulation.java
@@ -1,6 +1,20 @@
+import java.util.LinkedList;
+
 
 public class DonneesSimulation {
   private Carte carte;
-  // private ListRobot listRobot;
-  // private ListIncendie listIncendie;
+  private LinkedList<Robot> listeRobot;
+  private LinkedList<Incendie> listeIncendie;
+
+  public DonneesSimulation(){
+    this.carte = new Carte(50, 100);
+    this.listeRobot = new LinkedList<Robot>();
+    this.listeIncendie = new LinkedList<Incendie>();
+  }
+
+  public DonneesSimulation(int nbLignes, int nbColonnes){
+    this.carte = new Carte(nbLignes, nbColonnes);
+    this.listeRobot = new LinkedList<Robot>();
+    this.listeIncendie = new LinkedList<Incendie>();
+  }
 }
