002d45831d1c2fd43ff4b05fadc010e799271227
Robin Canis <robin.canis@grenoble-inp.org>
2022-11-23 11:22:51 +0000

    Upload New File

---
+++ VRAC/pshm_ucase_send.c
@@ -0,0 +1,74 @@
+/* pshm_ucase_send.c
+
+Licensed under GNU General Public License v2 or later.
+*/
+#include <string.h>
+#include "pshm_ucase.h"
+
+int main(int argc, char *argv[]) {
+    // if (argc != 3) {
+    //     fprintf(stderr, "Usage: %s /shm-path strings\n", argv[0]);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    char *shmpath = argv[1];
+    int nb_words = argc-2;
+
+    
+
+    /* Open the existing shared memory object and map it
+        into the caller's address space. */
+
+    int fd = shm_open(shmpath, O_RDWR, 0);
+    if (fd == -1)
+        errExit("shm_open");
+
+    struct shmbuf *shmp = (shmbuf*)mmap(NULL, sizeof(*shmp),
+                                PROT_READ | PROT_WRITE,
+                                MAP_SHARED, fd, 0);
+    if (shmp == MAP_FAILED)
+        errExit("mmap");
+
+
+    memcpy(&shmp->buf, (char*)&nb_words, (size_t)1);
+    if (sem_post(&shmp->sem1) == -1)
+        errExit("sem_post");
+     if (sem_wait(&shmp->sem2) == -1)
+        errExit("sem_wait");
+
+
+    /* Copy data into the shared memory object. */
+    int i;
+    for(i=0; i<nb_words;i++){
+
+        char *string = argv[i+2];
+        size_t len = strlen(string);
+
+        if (len > BUF_SIZE) {
+        fprintf(stderr, "String is too long\n");
+        exit(EXIT_FAILURE);
+        }
+
+        shmp->cnt = len;
+        
+        memcpy(&shmp->buf, string, len);
+
+        /* Tell peer that it can now access shared memory. */
+
+        if (sem_post(&shmp->sem1) == -1)
+            errExit("sem_post");
+
+        /* Wait until peer says that it has finished accessing
+            the shared memory. */
+
+        if (sem_wait(&shmp->sem2) == -1)
+            errExit("sem_wait");
+
+        /* Write modified data in shared memory to standard output. */
+
+        write(STDOUT_FILENO, &shmp->buf, len);
+        write(STDOUT_FILENO, "\n", 1);
+    }
+
+    exit(EXIT_SUCCESS);
+}
